eab46d2da77fdf110892492e144d4f276729b167
KaiserIrfan <irfanuddinmartadza@gmail.com>
2025-05-26 21:24:59 +0800

    connect Firebase to the app

--- Application/SmartBin/app/src/main/java/com/example/smartbin/MainActivity.java
+++ Application/SmartBin/app/src/main/java/com/example/smartbin/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.smartbin;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
+
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView fullness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbref = database.getReference("Readings");
+        fullness = findViewById(R.id.Fullness);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        dbref.child("Fullness").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Object value = snapshot.getValue();
+                if (value != null){
+                    fullness.setText(value.toString());
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("Failed to read value.", error.toException());
+            }
         });
     }
 }
