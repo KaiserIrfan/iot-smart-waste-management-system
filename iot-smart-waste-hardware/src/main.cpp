8257a371685d1b341e25e863e618ccda86f84fc5
Jannik Bubeck <jbubeck@stud.hs-offenburg.de>
2025-06-04 18:20:38 +0800

    Refactor to make extensive use of functions for readability

--- iot-smart-waste-hardware/src/main.cpp
+++ iot-smart-waste-hardware/src/main.cpp
@@ -1,3 +1,6 @@
+// -----------------
+// --- Libraries ---
+// -----------------
 #include <Arduino.h>
 #include <WiFi.h> // Include the WiFi library for ESP32
 #include <WiFiManager.h> // Include the WiFi library for ESP32
@@ -5,31 +8,98 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-// Define pins for the ESP32
+// ---------------------
+// --- Configuration ---
+// ---------------------
+
+// --- Pins ---
 #define TriggerPin 23 // Define the trigger pin
 #define LED_BUILTIN 2 // Define the built-in LED pin for ESP32
 
+// --- Firebase config ---
 #define API_KEY "AIzaSyDN0bocHyMBXdRX7nLLn9TRyZ6pghbHqfI" // Firebase API key
 #define DATABASE_URL "https://smart-waste-management-1b537-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
+// ------------------------
+// --- Global variables ---
+// ------------------------
+
+// --- Firebase data ---
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
+
+// --- Firebase status ---
 bool firebaseConnected = false; // Variable to check if Firebase is connected
-unsigned long previousMillis = 0; // Variable to store the last data send/read
 
-// put function declarations here:
-void checkWiFiTrigger(); // Function to check the trigger pin and start WiFi configuration
+// --- Timing ---
+unsigned long previousTime = 0; // Variable to store the last data send/read
 
+// -----------------------------
+// --- Function declarations ---
+// -----------------------------
+
+
+// --- Setup functions ----
+
+void pinsSetup();
+void wifiSetup();
+void firebaseSetup();
+
+// --- Loop functions ---
+
+// Helper functions
+bool updateNeeded();
+
+// Hardware functions
+
+// IoT function
+void wifiCheckTrigger(); // Function to check the trigger pin and start WiFi configuration
+void wifiFirebaseConnectionCheck();
+bool firebaseReadLidTrigger();
+
+
+// -------------
+// --- Main ----
+// -------------
+
+// --- Setup, run once at boot ---
 void setup() {
-  // put your setup code here, to run once:
-  WiFi.mode(WIFI_STA); // Set the WiFi mode to Station
-  WiFiManager wm;
   Serial.begin(115200);
+  pinsSetup();
+  wifiSetup();
+  firebaseSetup();
+}
 
-  //pinMode:
+// --- Loop, run repeatedly until shutdown ---
+void loop() {
+
+  // Perform IoT connection check
+  // --> Check if user wants to enter WiFi setup mode
+  // --> Check if connected to WiFi
+  // --> Check if connected to Firebase
+  wifiCheckTrigger(); // Check if user want to configure WiFi
+  wifiFirebaseConnectionCheck(); // Check if WiFi and Firebase is connected
+
+  // Read data
+  bool firebaseTriggerLid = firebaseReadLidTrigger();
+}
+
+
+// ------------------------
+// --- Helper functions ---
+// ------------------------
+
+// --- Setup functions ---
+
+void pinsSetup() {
   pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
   pinMode(TriggerPin, INPUT_PULLUP); // Initialize the TriggerPin as an input
+}
+
+void wifiSetup() {
+  WiFi.mode(WIFI_STA); // Set the WiFi mode to Station
+  WiFiManager wm;
 
   if (!wm.autoConnect("ESP32_WiFi_Config")) {
     Serial.println("Failed to connect to WiFi. Starting Config Portal...");
@@ -43,8 +113,9 @@ void setup() {
     Serial.println(WiFi.localIP()); // Print the local IP address
     digitalWrite(LED_BUILTIN, HIGH); // LED indicates WiFi is connected
   }
+}
 
-  // Initialize Firebase
+void firebaseSetup() {
   config.api_key = API_KEY; // Set the Firebase API key
   config.database_url = DATABASE_URL; // Set the Firebase database URL
   if(WiFi.isConnected()) {
@@ -64,33 +135,19 @@ void setup() {
   }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(!WiFi.isConnected()) { // Check if WiFi is not connected
-    Serial.println("WiFi is not connected");
-    digitalWrite(LED_BUILTIN, LOW); // LED indicates WiFi is not connected
-  } else {
-    digitalWrite(LED_BUILTIN, HIGH); // LED indicates WiFi is connected
-    if(auth.token.uid != ""){
-      firebaseConnected = true; // Set firebaseConnected to true if UID is not empty
-    } else {firebaseConnected = false;}
-  }
-  checkWiFiTrigger(); // Check if user want to configure WiFi
+// --- Loop functions ---
 
-  // Read database
-  if(Firebase.ready() && firebaseConnected == true && millis() - previousMillis > 5000){
-    previousMillis = millis(); // update the last data send/read time
-    if (Firebase.RTDB.getInt(&fbdo, "/Readings/Fullness")) { // Read the "Fullness" value from Firebase
-      Serial.print("Fullness: ");
-      Serial.println(fbdo.intData()); // Print the fullness value
-    } else {
-      Serial.println("Failed to read Fullness value from Firebase.");
-    }
+bool updateNeeded(ulong frequency) {
+  ulong currentTime = millis();
+  ulong passedTime = currentTime - previousTime;
+  if(passedTime > frequency) {
+    previousTime = currentTime;
+    return true;
   }
+  return false;
 }
 
-// put function definitions here:
-void checkWiFiTrigger() {
+void wifiCheckTrigger() {
   // This function checks the trigger pin
   if (digitalRead(TriggerPin) == LOW) { // If the trigger pin is LOW
     Serial.println("WiFi setting activated!"); 
@@ -108,4 +165,30 @@ void checkWiFiTrigger() {
       Serial.println("Failed to start WiFi Config Portal.");
     }
   } 
+}
+
+void wifiFirebaseConnectionCheck() {
+    if(!WiFi.isConnected()) { // Check if WiFi is not connected
+    Serial.println("WiFi is not connected");
+    digitalWrite(LED_BUILTIN, LOW); // LED indicates WiFi is not connected
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH); // LED indicates WiFi is connected
+    if(auth.token.uid != ""){
+      firebaseConnected = true; // Set firebaseConnected to true if UID is not empty
+    } else {firebaseConnected = false;}
+  }
+}
+
+bool firebaseReadLidTrigger() {
+  if(Firebase.ready() && firebaseConnected == true && updateNeeded(5000)){
+    if (Firebase.RTDB.getBool(&fbdo, "/open_lid")) {
+      Serial.print("Open Lid: ");
+      if (fbdo.boolData()) {
+        Serial.println("true"); // Print true if the lid is open
+        return fbdo.boolData();
+      }
+      Serial.println("false"); // Print false if the lid is closed
+    }
+  }
+  return false;
 }
